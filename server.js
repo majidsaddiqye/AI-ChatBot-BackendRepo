90e0c77717b8a373051c64e81826a5798b13ea2b
Abdul Majid <majidsaddiqye@gmail.com>
2025-11-28 21:49:48 +0500

    integrate Gemini Ai and handle its response also

--- server.js
+++ server.js
@@ -2,13 +2,23 @@ require("dotenv").config();
 const app = require("./src/app");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const generateResponse = require("./src/services/ai.service");
 
 PORT = process.env.PORT;
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
 io.on("connection", (socket) => {
-  // ...
+  console.log("A User Connected");
+
+  socket.on("ai-message", async (data) => {
+    const response = await generateResponse(data.prompt);
+    socket.emit("ai-message-response", {response})
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A User DisConnected");
+  });
 });
 
 httpServer.listen(PORT, () => {
