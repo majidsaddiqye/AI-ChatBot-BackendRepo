e7b9baa4f2df945550853f7b32d859067495c52b
Abdul Majid <majidsaddiqye@gmail.com>
2025-11-30 00:33:54 +0500

    Implement Cors

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
 const app = require("./src/app");
+const cors = require('cors')
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const generateResponse = require("./src/services/ai.service");
 
 PORT = process.env.PORT;
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173", // Aapke Frontend ka URL (Vite default)
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
 
 const chatHistory = [];
 
@@ -14,7 +21,7 @@ io.on("connection", (socket) => {
   console.log("A User Connected");
 
   socket.on("ai-message", async (data) => {
-    
+
     chatHistory.push({
       role: "user",
       parts: [{ text: data }],
